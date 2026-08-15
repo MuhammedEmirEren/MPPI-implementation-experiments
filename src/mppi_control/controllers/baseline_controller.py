764f7c94e8e86980c3a089e70c870b5c817a55d4
“MuhammedEmirEren” <emir4200@hotmail.com>
2026-08-15 17:18:56 +0300

    Initial pendulum dynamics estimator

---
+++ src/mppi_control/controllers/baseline_controller.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class PendulumPDController:
+    def __init__( self, kp: float, kd: float, max_torque: float,) -> None:
+        
+
+    def reset(self) -> None:
+        
+
+    def act(self, observation: np.ndarray) -> np.ndarray:
+       
